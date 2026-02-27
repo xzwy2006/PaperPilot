566e76c60dfacc04ffc50e5c46b7634948a517bf
Zhichen Wang <81145693+xzwy2006@users.noreply.github.com>
2026-02-27 17:10:41 +0800

    feat(exporter): add exporters package __init__ with export_ris re-export

---
+++ paperpilot/core/exporters/__init__.py
@@ -0,0 +1,9 @@
+"""
+paperpilot.core.exporters
+~~~~~~~~~~~~~~~~~~~~~~~~~
+Export utilities for PaperPilot screening results.
+"""
+
+from paperpilot.core.exporters.ris import export_ris
+
+__all__ = ["export_ris"]
